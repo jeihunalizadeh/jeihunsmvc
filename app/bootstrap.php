2c9402dc899adfc17d1cbd9a1d02c508c788abe4
jeihunweb <jeyhun.alizadeh@gmail.com>
2019-04-03 23:21:53 +0500

    Core class development, Making link structure, bug fixes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,12 @@
-<?php echo "HELLO WORLD"; ?>
+<?php 
+
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
+
+
+
+
+
+
+ ?>
--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
-<?php require_once('..app/bootstrap.php'); ?>
+<?php
+
+require_once'../app/bootstrap.php';
+
+  // Init Core Library
+  $init = new Core;
+ ?>
